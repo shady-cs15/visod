a6bb529a81d80630e14a4b384689c4b7e4938949
Satyaki Chakraborty <satyaki.cs15@gmail.com>
2015-10-15 22:02:52 +0530

    Add CMake script

--- odo.cpp
+++ odo.cpp
@@ -27,17 +27,19 @@ THE SOFTWARE.
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/calib3d/calib3d.hpp"
+#include "math.h"
 
-#define SEQ_MAX 288
+#define SEQ_MAX 111
+#define PI 3.14159265
 
 using namespace std;
 using namespace cv;
 
 string get_sequence(int n) {
-	if (n==0) return "00000";
-	else if (n/10 == 0) return "0000"+to_string(n);
-	else if (n/100 == 0) return "000"+to_string(n);
-	else if (n/1000 == 0) return "00"+to_string(n);
+	if (n==0) return "000000";
+	else if (n/10 == 0) return "00000"+to_string(n);
+	else if (n/100 == 0) return "0000"+to_string(n);
+	else if (n/1000 == 0) return "000"+to_string(n);
 	return NULL;
 }
 
@@ -57,10 +59,9 @@ void ratioTest(vector<vector<DMatch> > &matches, vector<DMatch> &good_matches) {
 int main() {
 	int seq_id = 0, scene_id = 0;
 	Mat cur_frame, prev_frame, cur_frame_kp, prev_frame_kp;
-	cur_frame = imread("../data/00000-ladybug-0.jpg").t();
-	resize(cur_frame, cur_frame, Size(), 0.4, 0.4);
+	cur_frame = imread("../2010_03_09_drive_0023/I1_000000.png");
+	//resize(cur_frame, cur_frame, Size(), 0.4, 0.6);
 	cvtColor(cur_frame, cur_frame, CV_BGR2GRAY);
-	int minHessian = 400;
 	vector<KeyPoint> keypoints_1, keypoints_2, good_keypoints_1, good_keypoints_2;
 	vector<vector<DMatch> > matches;
 	vector<DMatch> good_matches;
@@ -79,12 +80,11 @@ int main() {
 
 	Mat_<double> pose = (Mat_<double>(4, 1) << 0.00, 0.00, 0.00, 1.00);
 	Mat_<double> i4 = (Mat_<double>(1, 4) << 0.00, 0.00, 0.00, 1.00);
-	cout << pose << "\n";
 
 	for (int i=1; i<=SEQ_MAX; i++) {
 		cur_frame.copyTo(prev_frame);
-		cur_frame = imread("../data/"+get_sequence(i)+"-ladybug-0.jpg").t();
-		resize(cur_frame, cur_frame, Size(), 0.4, 0.4);
+		cur_frame = imread("../2010_03_09_drive_0023/I1_"+get_sequence(i)+".png");
+		//resize(cur_frame, cur_frame, Size(), 0.4, 0.6);
 		cvtColor(cur_frame, cur_frame, CV_BGR2GRAY);
 	
 		keypoints_1 = keypoints_2;
@@ -106,11 +106,11 @@ int main() {
                	vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
 
 		// TODO track features
+		// (if needed)
 		
 
 		// Retrieve 2D points from good_matches
 		// Compute Essential Matrix, R & T
-		// TODO Change hardcoded values of f and p
 		good_keypoints_1.clear();
 		good_keypoints_2.clear();
 		point1.clear();
@@ -126,20 +126,33 @@ int main() {
 		KeyPoint::convert(good_keypoints_1, point1, vector<int>());
 		KeyPoint::convert(good_keypoints_2, point2, vector<int>());
 
-		double f = 718.8560;
-		Point2f pp(607.1928, 185.2157);
-		Mat E, R, t, T, mask;
+		double f = (double)(8.941981e+02 + 8.927151e+02)/2;
+		Point2f pp((float)6.601406e+02, (float)2.611004e+02);
+		Mat E, R, t, T;
 		if (point1.size() >5 && point2.size() > 5) {
 			E = findEssentialMat(point2, point1, f, pp, RANSAC, 0.999, 1.0);
 			recoverPose(E, point2, point1, R, t, f, pp);
 			hconcat(R, t, T);
   			vconcat(T, i4, T);
   			pose = T*pose;
-  			cout << pose.t() << "\n";
+  			cout << pose.t() << "\n" ;
+
+  			// Euler angles
+  			/*
+  			double alpha_1 = atan2(R.at<double>(1,2), R.at<double>(2,2)) * 180 / PI;
+  			cout << "[" << alpha_1 << ", ";
+  			double c = sqrt(R.at<double>(0,0)*R.at<double>(0,0) + R.at<double>(0,1)*R.at<double>(0,1));
+  			double alpha_2 = atan2(-R.at<double>(0,2), c) * 180 / PI;
+  			cout << alpha_2 << ", ";
+  			double s1 = sin(alpha_1);
+  			double c1 = cos(alpha_1);
+  			double alpha_3 = atan2(s1*R.at<double>(2,0)-c1*R.at<double>(1,0),c1*R.at<double>(1,1)-s1*R.at<double>(2,1)) * 180 /PI; 
+  			cout << alpha_3 << "]\n";*/
   		}
 
+  		resize(img_matches, img_matches, Size(), 0.4, 0.6);
   		imshow("matches", img_matches);
-		if (waitKey(33) == 27) break;
+		if (waitKey(0) == 27) break;
 	}
 	return 0;
 }
